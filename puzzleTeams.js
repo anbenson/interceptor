4fa964788f6b1864c2e6d0a3e565064dd6e5d055
Andrew Benson <adbenson@cmu.edu>
2015-03-13 14:33:45 -0700

    added server-side registration code

---
+++ puzzleTeams.js
@@ -0,0 +1,87 @@
+// object that keeps track of socket-team associations
+// register sockets with setPlayer and setObserver
+// lookup teams with lookup and delete sockets with delete
+// please do not access socketMap and teams directly
+function PuzzleTeams() {
+  // maps socket ids to teams
+  this.socketMap = {};
+  // list of registered teams. should be read-only, except for structs within.
+  this.teams = [{"iden": "test", "currPuzzle":"0   ## #     ## #  #### X",
+                 "player": null, "observer": null}];
+  // most methods return a boolean (whether they succeeded)
+  
+  // given a socket, find the corresponding team if it exists
+  this.lookup = function(socket) {
+    if (socket.id in this.socketMap) {
+      return this.socketMap[socket.id];
+    }
+    return null;
+  };
+  // given a socket, remove the socket
+  this.delete = function(socket) {
+    if (socket.id in this.socketMap) {
+      var struct = this.socketMap[socket.id];
+      if (socket.id === struct.player.id) {
+        struct.player = null;
+      }
+      else {
+        struct.observer = null;
+      }
+      delete this.socketMap[socket.id];
+      return true;
+    }
+    return false;
+  };
+  // given a socket and identifier, register the socket as a player
+  this.setPlayer = function(socket, iden) {
+    // check socket isn't already registered and team exists without player
+    if (socket.id in this.socketMap) {
+      return false;
+    }
+    var team = null;
+    for (var i = 0; i < this.teams.length; i++) {
+      if (this.teams[i].iden === iden) {
+        if (this.teams[i].player) {
+          return false;
+        }
+        team = this.teams[i];
+        break;
+      }
+    }
+    if (team === null) {
+      return false;
+    }
+    // register socket as player
+    team.player = socket;
+    this.socketMap[socket.id] = team;
+    return true;
+  };
+  // given a socket and identifier, register the socket as an observer
+  this.setObserver = function(socket, iden) {
+    // check socket isn't already registered and team exists without observer
+    if (socket.id in this.socketMap) {
+      return false;
+    }
+    var team = null;
+    for (var i = 0; i < this.teams.length; i++) {
+      if (this.teams[i].iden === iden) {
+        if (this.teams[i].observer) {
+          return false;
+        }
+        team = this.teams[i];
+        break;
+      }
+    }
+    if (team === null) {
+      return false;
+    }
+    // register socket as observer
+    team.observer = socket;
+    this.socketMap[socket.id] = team;
+    return true;
+  };
+}
+
+module.exports = function() {
+  return new PuzzleTeams();
+};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var puzzleTeams = require("./puzzleTeams")();
 
 // static files
 app.use("/js", express.static(__dirname+"/js"));
@@ -18,9 +19,45 @@ app.get("/", function(req, res) {
 });
 
 // socket.io routing
+// clients must be able to receive the following events:
+// puzzleError
+// clients must be able to send the following events:
+// register
 io.on("connection", function(socket) {
   console.log("new connection...");
+  // register: register socket with team in server records so we know who they
+  // are on future messages
+  socket.on("register", function(teamname, password, isPlayer) {
+    // check parameter types
+    if (typeof(teamname) != "string" || typeof(password) != "string" ||
+        typeof(isPlayer) != "boolean") {
+      socket.emit("puzzleError", "invalid parameters for register");
+      return;
+    }
+    // for now, ignore the password...
+    // register the client
+    var success;
+    if (isPlayer) {
+      success = puzzleTeams.setPlayer(socket, teamname);
+    }
+    else {
+      success = puzzleTeams.setObserver(socket, teamname);
+    }
+    // log results
+    if (!success) {
+      socket.emit("puzzleError", "could not register; are you already "+
+                    " registered, or do you have the wrong password?");
+      console.log("failed to register "+teamname+" with password "+password+
+                  " with "+(isPlayer?"player":"observer"));
+      return;
+    }
+    console.log("registered "+teamname+" with "+(isPlayer?"player":"observer"));
+  });
+  // disconnect: unregister socket in server records
   socket.on("disconnect", function() {
+    if (!puzzleTeams.delete(socket)) {
+      console.log("error in deleting "+socket.id);
+    }
     console.log("socket disconnected...");
   });
 });
