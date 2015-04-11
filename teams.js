35cf782020d887418188869fd7a94f6b2bef0f92
Andrew Benson <adbenson@cmu.edu>
2015-04-11 14:43:26 -0400

    add actual teams

--- teams.js
+++ teams.js
@@ -4,8 +4,8 @@
 
  module.exports = [
   {
-    "iden": "test",
-    "password": "test",
+    "iden": "test5000",
+    "password": "test5000",
     "player": null,
     "observer": null,
     "observerHint": [0,0],
@@ -13,8 +13,134 @@
     "inPlay": true
   },
   {
-    "iden": "test2",
-    "password": "test2",
+    "iden": "test3000",
+    "password": "test3000",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Plaut",
+    "password": "15112Puzzlehunt",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Qian",
+    "password": "a3136897",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Schroeder",
+    "password": "6d53794",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Reddy",
+    "password": "8003fc3",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Lao",
+    "password": "c143980",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Peraza",
+    "password": "75e42de",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Shi",
+    "password": "21be50ce",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Chhabra",
+    "password": "b7e4652",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Shanker",
+    "password": "f341702c",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Reichek",
+    "password": "d64fe05",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Gakhar",
+    "password": "81959574",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Choo",
+    "password": "92a03773b",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Korn",
+    "password": "e7528bc0d",
+    "player": null,
+    "observer": null,
+    "observerHint": [0,0],
+    "puzzleLevel": 0,
+    "inPlay": true
+  },
+  {
+    "iden": "Benson",
+    "password": "2a4c4098",
     "player": null,
     "observer": null,
     "observerHint": [0,0],
